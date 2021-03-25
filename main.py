e178e8cdab9804672ac34bf55328526670f4d41b
장진욱 <wfo3977@gmail.com>
2021-03-26 02:32:52 +0900

    reset

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
-from typing import Optional
+from enum import Enum
+
 from fastapi import FastAPI
-from pydantic import BaseModel
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 app = FastAPI()
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Optional[bool] = None
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
 
-@app.get("/")
-def read_boot():
-    return {"hi! FastAPI"}
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_price": item.price, "item_id": item_id}
+    return {"model_name": model_name, "message": "Have some residuals"}
